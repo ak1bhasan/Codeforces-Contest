eafd84e26754b77e4f662a099deadb5e846b0855
ak1bhasan <akibhasan011@gmail.com>
2026-01-30 00:17:59 +0600

    Adding CF Round 1077 Div. 2

---
+++ Contest CF/Codeforces Round 1077 (Div. 2)/B.cpp
@@ -0,0 +1,166 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int,int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+#define pq_max priority_queue<int> /// Max-heap
+#define pq_min priority_queue<int, vi, greater<int>> /// Min-heap
+#define umap unordered_map
+#define uset unordered_set
+
+
+
+#define PB push_back
+#define F first
+#define S second
+#define MP make_pair
+#define endl '\n'
+#define all(a) (a).begin(),(a).end()
+#define sz(x) (int)x.size()
+
+
+
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+
+
+#define MOD 1000000007
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define gcd(a,b) __gcd(a,b)
+#define sqr(a) ((a) * (a))
+
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+
+
+typedef vector<int>::iterator vit;
+typedef set<int>::iterator sit;
+
+
+/// Debugger
+
+
+template < typename F, typename S >
+ostream& operator << ( ostream& os, const pair< F, S > & p ) {
+    return os << "(" << p.first << ", " << p.second << ")";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const vector< T > &v ) {
+    os << "{";
+    for(auto it = v.begin(); it != v.end(); ++it) {
+        if( it != v.begin() ) os << ", ";
+        os << *it;
+    }
+    return os << "}";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const set< T > &v ) {
+    os << "[";
+    for(auto it = v.begin(); it != v.end(); ++it) {
+        if( it != v.begin() ) os << ", ";
+        os << *it;
+    }
+    return os << "]";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const multiset< T > &v ) {
+    os << "[";
+    for(auto it = v.begin(); it != v.end(); ++it) {
+        if( it != v.begin() ) os << ", ";
+        os << *it;
+    }
+    return os << "]";
+}
+
+template < typename F, typename S >
+ostream &operator << ( ostream & os, const map< F, S > &v ) {
+    os << "[";
+    for(auto it = v.begin(); it != v.end(); ++it) {
+        if( it != v.begin() ) os << ", ";
+        os << it -> first << " = " << it -> second ;
+    }
+    return os << "]";
+}
+
+#define dbg(args...) do {cerr << #args << " : "; debug_out(args); } while(0)
+
+void debug_out () {
+    cerr << endl;
+}
+
+template <typename T>
+void debug_out( T a[], int n ) {
+    for(int i = 0; i < n; ++i) cerr << a[i] << ' ';
+    cerr << endl;
+}
+
+template <typename T, typename ... Args>
+void debug_out( T arg, const Args &... rest) {
+    cerr << arg << ' ';
+    debug_out(rest...);
+}
+
+void solve()
+{
+    int n; cin >> n;
+    string s; cin >> s;
+
+    int cnt1 = count( all(s), '1' );
+
+    if( cnt1 == 0 ) {
+//        cout << ( n + 1 ) / 2 << endl;
+        cout << ( n + 2 ) / 3 << endl;
+        return;
+    }
+
+    int first = s.find('1'), last = s.rfind('1');
+
+    int add = 0;
+    add += ( first + 1 ) / 3;
+
+    for( int i = first; i < last; ) {
+        int j = i + 1, c = 0;
+        while( j <= last and s[j] == '0' ) {
+            c++;
+            j++;
+        }
+        add += c / 3;
+        i = j;
+//        dbg(add);
+//        dbg(i);
+    }
+
+    add += ( n - last ) / 3;
+//    dbg(add);
+
+    cout << cnt1 + add << endl;
+
+}
+
+int main()
+{
+    optimize();
+
+    int t; cin >> t;
+
+    while( t-- ) solve();
+
+    return 0;
+}
+
